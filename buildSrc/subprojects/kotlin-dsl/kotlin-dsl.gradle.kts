a4715e8eaff4d045b8802efa4757f4073be1b7a1
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-03-06 09:24:21 -0300

    Upgrade Kotlin DSL {0.18.4 => 0.18.5-SNAPSHOT}

--- buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
+++ buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    api("org.gradle:gradle-kotlin-dsl:0.18.4")
+    api("org.gradle:gradle-kotlin-dsl:0.18.5-SNAPSHOT")
     testImplementation("junit:junit:4.12")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
 }
