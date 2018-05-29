c8418834a2ff40ea9b1c08b07dffc167c573fd05
Bo Zhang <bo@gradle.com>
2018-05-29 17:37:46 +0800

    Ignore failures caused by Groovy incompatibility issue

--- buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/excluded-tests.kt
+++ buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/excluded-tests.kt
@@ -13,10 +13,6 @@ val excludedTests = listOf(
     "JacocoPluginMultiVersionIntegrationTest" to listOf(VERSION_1_9, VERSION_1_10),
     "JacocoPluginCoverageVerificationIntegrationTest" to listOf(VERSION_1_9, VERSION_1_10),
 
-    // Caused by: java.lang.IncompatibleClassChangeError: Method Person.getName()Ljava/lang/String; must be InterfaceMethodref constant
-    // Fail since build 125
-    "InterfaceBackedManagedTypeIntegrationTest" to listOf(VERSION_1_9, VERSION_1_10),
-
     // Cannot obtain Jvm arguments via java.lang.management.ManagementFactory.runtimeMXBean.inputArguments module java.management does not export sun.management to unnamed module @6427ecb
     "BuildEnvironmentModelCrossVersionSpec" to listOf(VERSION_1_9, VERSION_1_10), // "informs about java args as in the build script"
     "JavaConfigurabilityCrossVersionSpec" to listOf(VERSION_1_9, VERSION_1_10), // "customized java args are reflected in the inputArguments and the build model", "tooling api provided jvm args take precedence over gradle.properties"
--- subprojects/model-core/src/integTest/groovy/org/gradle/model/managed/InterfaceBackedManagedTypeIntegrationTest.groovy
+++ subprojects/model-core/src/integTest/groovy/org/gradle/model/managed/InterfaceBackedManagedTypeIntegrationTest.groovy
@@ -19,7 +19,10 @@ package org.gradle.model.managed
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.util.Requires
 import org.gradle.util.TestPrecondition
+import spock.lang.Issue
 
+@Issue("https://github.com/gradle/gradle/issues/721")
+@Requires(TestPrecondition.JDK8_OR_EARLIER)
 class InterfaceBackedManagedTypeIntegrationTest extends AbstractIntegrationSpec {
 
     def "rule method can define a managed model element backed by an interface"() {
