19b58a4345815de2b210718a3bc7ee4daa0fffcb
Stefan Wolf <wolf@gradle.com>
2018-07-11 09:56:38 +0200

    Remove `getContent` from PhysicalSnapshot

--- subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/mirror/logical/ClasspathFingerprintingStrategy.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/mirror/logical/ClasspathFingerprintingStrategy.java
@@ -152,7 +152,7 @@ public class ClasspathFingerprintingStrategy implements FingerprintingStrategy {
 
         @Override
         public void visit(PhysicalSnapshot fileSnapshot) {
-            if (fileSnapshot.getContent().getType() == FileType.RegularFile) {
+            if (fileSnapshot.getType() == FileType.RegularFile) {
                 HashCode normalizedContent = fingerprintFile((PhysicalFileSnapshot) fileSnapshot);
                 if (normalizedContent != null) {
                     delegate.visit(fileSnapshot, normalizedContent);
@@ -173,8 +173,7 @@ public class ClasspathFingerprintingStrategy implements FingerprintingStrategy {
             if (shouldBeIgnored) {
                 return null;
             }
-            HashCode newHash = classpathResourceHasher.hash(fileSnapshot);
-            return newHash;
+            return classpathResourceHasher.hash(fileSnapshot);
         }
 
         @Override
@@ -189,7 +188,7 @@ public class ClasspathFingerprintingStrategy implements FingerprintingStrategy {
         if (FileUtils.hasExtensionIgnoresCase(fileSnapshot.getName(), ".jar")) {
             return snapshotJarContents(fileSnapshot);
         }
-        return nonJarFingerprintingStrategy.determineNonJarFingerprint(fileSnapshot.getContent().getContentMd5());
+        return nonJarFingerprintingStrategy.determineNonJarFingerprint(fileSnapshot.getContentHash());
     }
 
     @Nullable
